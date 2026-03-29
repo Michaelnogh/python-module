3e0118f6a1a2e3aff232c67b503730cff2edc901
Michael Nogh <michaelnogh@gmail.com>
2026-03-29 10:09:05 +0300

    Adding While-loops lession

---
+++ loops/while_loops.py
@@ -0,0 +1,18 @@
+# while loops basics
+#-------------------------------------------
+
+# user_input = input("Would you like to play? (n/y) :")
+
+# while user_input != "n": 
+#     print("Game is Running")
+#     user_input = input("Would you liketo play again (n/y): ")
+
+
+
+
+while True:
+    user_input = input("would you like to play? (n/y): ")
+
+    if user_input == "n":
+        break
+    print("Game is running")
